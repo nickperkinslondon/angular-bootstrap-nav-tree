f72801a04062bd97e7b50c5b3543ab56255aacbc
John McTeague <john.mcteague@gmail.com>
2013-12-13 21:11:27 +0000

    Issue #17 - IE8 Support

--- dist/abn_tree_directive.js
+++ dist/abn_tree_directive.js
@@ -6,6 +6,7 @@ module.directive('abnTree', function($timeout) {
   return {
     restrict: 'E',
     templateUrl: '../dist/abn_tree_template.html',
+    replace: true,
     scope: {
       treeData: '=',
       onSelect: '&',
