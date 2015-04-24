53749d29dd308da24e15cbc1d0557a8b4ef5c756
Dotan Simha <dotansimha@gmail.com>
2015-04-24 16:56:05 +0300

    Fix dep for bootstrap

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@0.9.0.1');
 
-  api.use('twbs:bootstrap', 'client');
+  api.use('twbs:bootstrap@3.3.4', 'client');
   api.use('angularjs:angular@1.3.15', 'client');
 
   api.addFiles('dist/abn_tree_directive.js', 'client');
